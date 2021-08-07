f19accea5bdd323f24eb313f25c349de7096a1dd
Abbin44 <abbin44@gmail.com>
2021-08-08 00:41:08 +0200

    Added many new features

--- Rom Sorter/MainWindow.xaml.cs
+++ Rom Sorter/MainWindow.xaml.cs
@@ -4,18 +4,32 @@ using System.Windows;
 using WinForms = System.Windows.Forms;
 using System.IO;
 using System.Linq;
+using System.Windows.Media;
+using System.Reflection;
+using System.Text;
+using System.Diagnostics;
 
 namespace Rom_Sorter
 {
     public partial class MainWindow : Window
     {
+        Color color;
+        Brush foreColor;
+        string filePath;
+        string extFilePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\Extentions.ext";
+        bool? searchRecursivley = false;
+        bool deleteFiles = true;
+
         public MainWindow()
         {
             InitializeComponent();
+            color = (Color)ColorConverter.ConvertFromString("#FF0197FF");//Very janky but very beautiful convertions here
+            foreColor = new SolidColorBrush(color);
             regionMenu.SelectedIndex = 0;
+            foreColor.Opacity = 0.3f;
+            extTextBox.Foreground = foreColor;
         }
 
-        string filePath;
         private void browseDir_Click(object sender, RoutedEventArgs e)
         {
             WinForms.FolderBrowserDialog folderDlg = new WinForms.FolderBrowserDialog();
@@ -44,9 +58,9 @@ namespace Rom_Sorter
                 MessageBox.Show("Please select a region", "Error");
                 return;
             }
-
-            string region = regionMenu.SelectedItem.ToString();
-
+            string region = regionMenu.SelectedValue.ToString();
+            string[] tokens = region.Split(':');
+            region = tokens[1].Trim();
             switch (region)
             {
                 case "Europe":
@@ -72,12 +86,15 @@ namespace Rom_Sorter
 
         public void RunScan(string region)
         {
+            //TODO: Only search stored file extentions
             List<string> regions = new List<string>();
             List<string> allRegs = new List<string>();
 
             bool other = false;
             bool keep = false;
 
+            searchRecursivley = RecursiveSearchChk.IsChecked;
+            filePath = filePathBox.Text;
             if (region == "E")
             {
                 regions.Add("(E)");
@@ -112,22 +129,50 @@ namespace Rom_Sorter
                 allRegs.Add("(Japan)");
             }
 
-            filePath = filePathBox.Text;
-            DirectoryInfo di = new DirectoryInfo(filePath);
+            string[] files;
+            if (searchRecursivley == true)
+                files = Directory.GetFiles(filePath, "*", SearchOption.AllDirectories);
+            else
+                files = Directory.GetFiles(filePath, "*", SearchOption.TopDirectoryOnly);
 
-            foreach (FileInfo file in di.EnumerateFiles())
+            string[] extentions = File.ReadAllLines(extFilePath);
+            string[] tokens;
+            bool isROM = false;
+            for (int i = 0; i < extentions.Length; ++i)
+            {
+                tokens = extentions[i].Split(';');
+                extentions[i] = tokens[0];
+            }
+
+            for (int i = 0; i < files.Length; ++i)
             {
                 if (other == false)
                 {
-                    keep = regions.Any(file.Name.Contains);
-                    if (keep == false)
-                        file.Delete();
+                    for (int j = 0; j < extentions.Length; j++)
+                    {
+                        if (files[i].Contains(extentions[j]))
+                        {
+                            keep = regions.Any(files.Contains);
+                            if (keep == false)
+                                File.Delete(files[i]);
+                        }
+                        else
+                            continue;
+                    }
                 }
                 else if (other == true)
                 {
-                    keep = allRegs.Any(file.Name.Contains);
-                    if (keep == true)
-                        file.Delete();
+                    for (int j = 0; j < extentions.Length; j++)
+                    {
+                        if (files[i].Contains(extentions[j]))
+                        {
+                            keep = allRegs.Any(files.Contains);
+                            if (keep == true)
+                                File.Delete(files[i]);
+                        }
+                        else
+                            continue;
+                    }
                 }
             }
         }
@@ -144,6 +189,38 @@ namespace Rom_Sorter
 
                 File.Move(file, filePath + name);
             }
+        }
+
+        public void AddExtention(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(extTextBox.Text) || extTextBox.Text == ".example")
+            {
+                MessageBox.Show("The extention field is empty", "Error");
+                return;
+            }
+            string extention = extTextBox.Text;
+
+            StringBuilder sbe = new StringBuilder();
+            sbe.Append("\n");
+            if (!extention.StartsWith("."))
+                sbe.Append(".");
+
+            sbe.Append(extention);
+            sbe.Append(";");
+            File.AppendAllText(extFilePath, sbe.ToString());
+            extTextBox.Text = string.Empty;
+        }
+
+        private void extTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            extTextBox.Text = string.Empty;
+            foreColor.Opacity = 1.0f;
+            extTextBox.Foreground = foreColor;
+        }
+
+        private void OpenExtentionFile(object sender, RoutedEventArgs e)
+        {
+            Process.Start("notepad.exe", extFilePath);
         }
     }
 }
